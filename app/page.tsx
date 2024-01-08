78fda2f1d596d4ae5f683ab1826f8509d027001c
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-01-08 12:19:46 +0530

    feat: change image in profile

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
       <div className="absolute z-[2] right-0 top-0 h-full w-full">
         <Particle />
       </div>
-      <div className="absolute right-10 md:right-40 bottom-0 z-[10]">
+      <div className="absolute bottom-0 lg:bottom-auto right-[4.3rem] md:right-[5.4rem] lg:right-[7.4rem] xl:right-52 z-[10]">
         <Image
-          src="/MoustacheMan.png"
-          alt="MoustacheMan"
+          src="/BishalSinghDeo.png"
+          alt="BishalSinghDeo"
           width={560}
-          height={560}
-          className="w-[300px] h-[300px] z-[1] md:h-[560px] md:w-[560px]"
+          height={260}
+          className="w-[220px] h-[220px] z-[1] lg:w-[300px] lg:h-[300px] xl:h-[400px] xl:w-[400px] rounded-full  border-[6px] border-orange-600 hover:bg-yellow-900"
         />
       </div>
       <Image
@@ -24,10 +24,20 @@ export default function Home() {
         className="absolute left-0 top-0"
       />
 
-      <div className="flex flex-col gap-3 z-[10] pl-5 md:pl-40">
-        <h1 className="text-[50px] text-white max-w-[500px]"> Transforming Ideas into{" "}
-         <span className="text-red-600">Digital Reality</span>
+      <div className="flex flex-col gap-3 z-[10] pl-5 md:pl-40 pt-32">
+        <h1 className="text-[50px] text-white max-w-[550px]"> Frontend{" "}
+         <span className="text-red-600">Developer👋</span>
+         <p className='text-white text-lg'>Hi, I am Bishal, passionate frontend developer experience in creating Web application, Mobile application, API Integration and Software development. Check out my projects and skills.</p>
         </h1>
+        <div className="relative w-[100px] h-[100px] md:w-[160px] md:h-[160px] mt-5">
+          <Image
+            src="/assets/rounded-text.png"
+            alt="projects"
+            width={160}
+            height={160}
+            className="slow-spin hidden md:block"
+          />
+        </div>
       </div>
       
     </main>
