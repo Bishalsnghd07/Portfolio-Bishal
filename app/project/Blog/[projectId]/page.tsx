d8fc8eb03ad1f8a54a6e3ffb2fd4de22ca00dd12
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-01-16 13:34:01 +0530

    feat: added rotation

--- app/project/Blog/[projectId]/page.tsx
+++ app/project/Blog/[projectId]/page.tsx
@@ -23,10 +23,10 @@ const Slug = ({ params }: { params: { projectId: string } }) => {
 
   // Display the details on the page
   return (
-    <div className="flex items-center min-h-screen bg-cover bg-[#0C011A]">
+    <div className="flex items-center min-h-screen bg-cover bg-[#0C011A] perspective-1000">
       <div className="flex flex-wrap justify-center items-center gap-4 pt-[16rem] md:pt-[10rem] p-6 overflow-hidden">
         <div className="flex justify-center items-center h-[100%] w-[100%]">
-        {Slug && <img src={Slug.src} alt="Project Image" className="max-w-full max-h-full md:max-w-[80%] lg:max-w-[60%] xl:max-w-[40%]" /> }
+        {Slug && <img src={Slug.src} alt="Project Image"  className="max-w-full max-h-full md:max-w-[80%] lg:max-w-[60%] xl:max-w-[40%] custom-rotation" /> }
         </div>
       <h1 className=" text-white text-[2.9rem]">
         {Slug?.details}     
