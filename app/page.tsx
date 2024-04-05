56ede56eb023c342c1ae8e0498cf8b6f27e669d2
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-04-05 14:16:43 +0530

    feat: modification on main component

--- app/page.tsx
+++ app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
       </div>    
 
       <div className='grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center'>
-        <motion.div className='flex flex-col justify-center items-start leading-7 p-5 md:pl-[10rem] md:max-w-[40rem] pt-[8rem] md:pt-[1.2rem]' variants={textVariants} initial="initial" animate="animate">
+        <motion.div className='flex flex-col justify-center items-start leading-7 p-5 md:pl-[10rem] md:max-w-[40rem] pt-[8rem] md:pt-[5rem] lg:pt-[1.2rem]' variants={textVariants} initial="initial" animate="animate">
           <h1 className='text-5xl lg:text-6xl text-white transition md:hover:translate-x-4 duration-500 cursor-pointer md:z-20'>Frontend{" "}
           <span className='text-red-600'>Developer {isSmallScreen ? '' : '👋'}</span>
           </h1>
